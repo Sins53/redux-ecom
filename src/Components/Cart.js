30d94ad416c296486a19c01ded2ef3d5a2b423cc
Sahil Shrestha <shresthasahil27@gmail.com>
2022-03-11 16:04:01 +0545

    fixed prroduct page Bug

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -1,57 +1,69 @@
-import React from 'react'
-import { useDispatch, useSelector } from 'react-redux';
-import {AiFillCloseCircle} from 'react-icons/ai'
-import { removeItem } from "../redux/actions/cart"
-import Adder from './Adder'
-import { resetOrder } from '../redux/actions/adder';
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { AiFillCloseCircle } from "react-icons/ai";
+import { removeItem } from "../redux/actions/cart";
+import Adder from "./Adder";
+import { resetOrder } from "../redux/actions/adder";
 
-var stockName = '';
+var stockName = "";
 const Cart = () => {
   const cart = useSelector((state) => state.cart.cart);
   const dispatch = useDispatch();
 
-  const imgUrl = 'https://electronic-ecommerce.herokuapp.com/'
+  const imgUrl = "https://electronic-ecommerce.herokuapp.com/";
 
-  console.log(cart, 'here cart --------------')
+  console.log(cart, "here cart --------------");
 
   const removeCart = (id) => {
     dispatch(removeItem(id));
     dispatch(resetOrder(id));
-  }
-  
+  };
+
   return (
     <>
-    {cart.map((item) => {
-      {item.ordered <= 0 ? removeCart(item.id) : item.stock < 5 ? stockName = 'danger' : item.stock < 10 ? stockName = 'ok' : stockName = 'full'}
-      return (
-        <>
-        <div className="row mt-1" key={item.name}>
-        <div className="col-auto">
-         <button className='btn btn-danger mt-3' onClick={() => removeCart(item.id)}> <AiFillCloseCircle /></button> 
-         </div>
-        <div className="col-auto">
-        <img className='Cart-img' src={imgUrl + item.image} alt="" />
-        </div>
-        <div className="col-4">
-          <h4>{item.name}</h4>
-          <h4>{'Rs. ' + item.r*item.ordered}</h4>
-        </div>
-        <div className="col-4 ml-auto text-end">
-          <h5 className={`ListCard-${stockName}`}>{ 'Stock remaining: ' + item.stock}</h5>
-          <Adder 
-          id = {item.id} 
-          stock = {item.stock}
-          ordered = {item.ordered}
-          />
-          <h5>Unit Price: {`Rs. ${item.r}`}</h5>
-          {/* <h5>ordered : {item.order}</h5> */}
-        </div>
-        </div>
-        </>
-      )
-    })}
+      {cart.map((item) => {
+        {
+          item.ordered <= 0
+            ? removeCart(item.id)
+            : item.stock < 5
+            ? (stockName = "danger")
+            : item.stock < 10
+            ? (stockName = "ok")
+            : (stockName = "full");
+        }
+        return (
+          <>
+            <div className="row mt-1" key={item.name}>
+              <div className="col-auto">
+                <button
+                  className="btn btn-danger mt-3"
+                  onClick={() => removeCart(item.id)}
+                >
+                  {" "}
+                  <AiFillCloseCircle />
+                </button>
+              </div>
+              <div className="col-auto">
+                <img className="Cart-img" src={imgUrl + item.image} alt="" />
+              </div>
+              <div className="col-4">
+                <h4>{item.name}</h4>
+                <h4>{"Rs. " + item.r * item.ordered}</h4>
+              </div>
+              <div className="col-4 ml-auto text-end">
+                <h5 className={`ListCard-${stockName}`}>
+                  {"Stock remaining: " + item.stock}
+                </h5>
+                <Adder id={item.id} stock={item.stock} ordered={item.ordered} />
+                <h5>Unit Price: {`Rs. ${item.r}`}</h5>
+                {/* <h5>ordered : {item.order}</h5> */}
+              </div>
+            </div>
+          </>
+        );
+      })}
     </>
-  )
-}
+  );
+};
 
-export default Cart
+export default Cart;
--- src/ECOM/PrductPage/index.js
+++ src/ECOM/PrductPage/index.js
@@ -1,17 +1,26 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import Adder from "../../Components/Adder";
 import Navbar from "../Navbar";
+import { addCart } from "../../redux/actions/cart";
 import { useParams } from "react-router-dom";
 import ListCard from "../../Components/ListCard";
 
 const ProductPage = () => {
   const { id } = useParams();
-  
-  const productList = useSelector((state) => state.product.products);
 
-  var id1 = Number(id)
-  var category = '';
+  const productList = useSelector((state) => state.product.products);
+  const orderValue = useSelector((state) => state.order);
+  const cart = useSelector((state) => state.cart.cart);
+
+  var id1 = Number(id);
+
+  // console.log(id);
+
+  const dispatch = useDispatch();
+  var category = "";
+
+  var imgUrl = "https://electronic-ecommerce.herokuapp.com/";
 
   var arr = productList.filter(checkFilter);
   var arr1 = productList.filter(categoryFilter);
@@ -19,7 +28,7 @@ const ProductPage = () => {
 
   function checkFilter(item) {
     if (item.id === id1) {
-      category = item.category[1]
+      category = item.category[1];
       return item;
     }
   }
@@ -29,50 +38,68 @@ const ProductPage = () => {
     }
   }
 
+  const toRs = (price) => {
+    var rs = price;
+    var arrRs = rs.split("$");
+    arrRs.shift();
+    var r = Number(arrRs[0]);
+    r *= 120;
+    return r;
+  };
+
+  const updateCart = (item) => {
+    Object.assign(item, { r: toRs(item.price), ordered: orderValue[item.id] });
+    var arr = [];
+    cart.forEach((citem) => {
+      if (item.id !== citem.id) {
+        arr.push(citem);
+      }
+    });
+    var unique = [item, ...arr];
+    dispatch(addCart(unique));
+  };
+
   return (
     <>
       <Navbar />
       <div className="container mt-5">
         {arr.map((item) => {
-            return (
-              <>
-                <div class="row justify-content-center">
-                  <div class="col-6">
-                    <img className="qwe" src={imgUrl + item.image} alt="" />
-                  </div>
-                  <div class="col-4">
-                    <div>
-                      <h2>{item.name}</h2>
-                      <h3>{item.price}</h3>
-                      <h3>Description :</h3>
-                      <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Fugit ut blanditiis id laboriosam. Repellat voluptatum
-                        a, sed fugiat, nihil perspiciatis, tempora veniam
-                        assumenda pariatur unde quidem sit quam quod numquam!
-                      </p>
-                      <Adder id={item.id} stock={item.stock} />
-                      <button
-                        className="ListCard-btn mt-4"
-                        onClick={() => updateCart(item)}
-                      >
-                        Add to Cart
-                      </button>
-                    </div>
+          return (
+            <>
+              <div class="row justify-content-center">
+                <div class="col-6">
+                  <img className="qwe" src={imgUrl + item.image} alt="" />
+                </div>
+                <div class="col-4">
+                  <div>
+                    <h2>{item.name}</h2>
+                    <h3>{item.price}</h3>
+                    <h3>Description :</h3>
+                    <p>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Fugit ut blanditiis id laboriosam. Repellat voluptatum a,
+                      sed fugiat, nihil perspiciatis, tempora veniam assumenda
+                      pariatur unde quidem sit quam quod numquam!
+                    </p>
+                    <Adder id={item.id} stock={item.stock} />
+                    <button
+                      className="ListCard-btn mt-4"
+                      onClick={() => updateCart(item)}
+                    >
+                      Add to Cart
+                    </button>
                   </div>
                 </div>
-                <div className="mt-5">
-                  <h2>Similar Items:</h2>
-                  <ListCard 
-                  arrReduced = {arrReduced}
-                  />
-                </div>
-              </>
-            );
+              </div>
+              <div className="mt-5">
+                <h2>Similar Items:</h2>
+                <ListCard arrReduced={arrReduced} />
+              </div>
+            </>
+          );
         })}
-        <br/> <br/> <br/> <br/>
+        <br /> <br /> <br /> <br />
       </div>
-      
     </>
   );
 };
